1a45b2fa997100a9d9675c979e3e0e7c3a961cd9
Ricardo <ricardo.elizaldeh@un.edu.mx>
2024-01-09 20:35:07 -0600

    Añadi mas codigo a Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Estos son los cambios realizadso");
+
+Console.WriteLine("Termine mi curso de Git");
+
+
